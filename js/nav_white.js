2a2dcd0114d2754d67f8de9f5f0cc20aa12d695e
root <root@DESKTOP-DROT0RD.localdomain>
2018-09-05 22:58:00 +0800

    change cname to flyoung.org

--- js/nav_white.js
+++ js/nav_white.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+  var scroll = $(window).scrollTop();
+  if (scroll > 0) {
+    $("nav").addClass("nav_white");
+  }
+
+  else{
+    $("nav").removeClass("nav_white");
+  }
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 0) {
