5e59cf23766f18b8ea586892dd00d451e7c03893
root <root@DESKTOP-DROT0RD.localdomain>
2018-09-07 01:23:21 +0800

    web level structure changed

--- js/nav_white.js
+++ js/nav_white.js
@@ -2,23 +2,23 @@ $(document).ready(function(){
   var scroll = $(window).scrollTop();
   if (scroll > 0) {
     $("nav").addClass("nav_white");
-    $('.logo').attr('src','../svg/logo.svg');
+    $('.logo').attr('src','/svg/logo.svg');
   }
 
   else{
     $("nav").removeClass("nav_white");
-    $('.logo').attr('src','../svg/logo_white.svg');
+    $('.logo').attr('src','/svg/logo_white.svg');
   }
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 0) {
 	    $("nav").addClass("nav_white");
-      $('.logo').attr('src','../svg/logo.svg');
+      $('.logo').attr('src','/svg/logo.svg');
 	  }
 
 	  else{
 		  $("nav").removeClass("nav_white");
-      $('.logo').attr('src','../svg/logo_white.svg');
+      $('.logo').attr('src','/svg/logo_white.svg');
 	  }
   })
 })
