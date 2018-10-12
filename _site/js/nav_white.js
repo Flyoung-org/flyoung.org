2f15e6de2a64766858dc2d1d8cf21148513c8b17
davidtheta <ddnslin@gmail.com>
2018-10-13 00:23:21 +0800

    uses Jekyll

---
+++ _site/js/nav_white.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  var scroll = $(window).scrollTop();
+  if (scroll > 0) {
+    $("nav").addClass("nav_white");
+    $('.logo').attr('src','/svg/logo.svg');
+  }
+
+  else{
+    $("nav").removeClass("nav_white");
+    $('.logo').attr('src','/svg/logo_white.svg');
+  }
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 0) {
+	    $("nav").addClass("nav_white");
+      $('.logo').attr('src','/svg/logo.svg');
+	  }
+
+	  else{
+		  $("nav").removeClass("nav_white");
+      $('.logo').attr('src','/svg/logo_white.svg');
+	  }
+  })
+})
